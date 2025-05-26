94fd0d9c2edd92f0351ad54a958ada8c885cfb9d
MaDes <madesappli@gmail.com>
2025-05-26 16:03:22 +0200

    Frame clickable

--- app/src/main/java/com/dev/testappcdastdenis/GameActivity.kt
+++ app/src/main/java/com/dev/testappcdastdenis/GameActivity.kt
@@ -1,6 +1,7 @@
 package com.dev.testappcdastdenis
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.fragment.app.FragmentActivity
 
 class GameActivity : FragmentActivity() {
@@ -8,12 +9,35 @@ class GameActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+        val frameTopLeft : FrameLayout = findViewById(R.id.fragment_container_topleft)
+        frameTopLeft.setOnClickListener(){
+            println("on a clické sur topleft bleu")
+        }
+        val frameTopRight : FrameLayout = findViewById(R.id.fragment_container_topright)
+        frameTopRight.setOnClickListener(){
+            println("on a clické sur frameTopRight rouge")
+        }
+        val frameBottomLeft : FrameLayout = findViewById(R.id.fragment_container_bottomleft)
+        frameBottomLeft.setOnClickListener(){
+            println("on a clické sur frameBottomLeft jaune")
+        }
+        val frameBottonRight : FrameLayout = findViewById(R.id.fragment_container_bottomright)
+        frameBottonRight.setOnClickListener(){
+            println("on a clické sur frameBottonRight vert")
+        }
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container_topleft, FirstFragment())
             .replace(R.id.fragment_container_topright, FirstFragment())
             .replace(R.id.fragment_container_bottomleft, FirstFragment())
             .replace(R.id.fragment_container_bottomright, FirstFragment())
-
             .commit()
+
+
+
     }
+
+
+
+
 }
