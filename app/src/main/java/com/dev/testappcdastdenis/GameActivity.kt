fb6663d25dd8bae2bbcfadaa2875c2260bde7e0a
MaDes <madesappli@gmail.com>
2025-05-27 12:13:00 +0200

    debut tours

--- app/src/main/java/com/dev/testappcdastdenis/GameActivity.kt
+++ app/src/main/java/com/dev/testappcdastdenis/GameActivity.kt
@@ -7,24 +7,11 @@ import androidx.fragment.app.FragmentActivity
 import com.dev.testappcdastdenis.utiljava.Utilitaires
 
 class GameActivity : FragmentActivity() {
+
+    private var sequence = mutableListOf<Int>()
+    var compteur = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        //choix aléatoire d'un frame
-            //méthode de random 0-3
-        val random = Utilitaires.generateRandom(4)
-
-
-        //enregistrement du choix (sequence)
-            //methode qui va renvoyer un frame à partir d'un chiffre
-            //activation bipColor à partir de la séquence
-
-        //activitation de la sequence de frame (bip)
-        //attente de reproduction par l'utilisateur
-        //validation ou non de la séquence
-        //recommence à partir de la séquence enregistrée ou à partir de zero
-
-
-
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -32,21 +19,25 @@ class GameActivity : FragmentActivity() {
         val frameTopLeft : FrameLayout = findViewById(R.id.fragment_container_topleft)
         frameTopLeft.setOnClickListener(){
             bipColor(frameTopLeft, R.color.blue)
+            compareSaisieSequence(0)
             println("on a clické sur topleft bleu")
         }
         val frameTopRight : FrameLayout = findViewById(R.id.fragment_container_topright)
         frameTopRight.setOnClickListener(){
             bipColor(frameTopRight, R.color.red)
+            compareSaisieSequence(1)
             println("on a clické sur frameTopRight rouge")
         }
         val frameBottomLeft : FrameLayout = findViewById(R.id.fragment_container_bottomleft)
         frameBottomLeft.setOnClickListener(){
             bipColor(frameBottomLeft, R.color.yellow)
+            compareSaisieSequence(2)
             println("on a clické sur frameBottomLeft jaune")
         }
         val frameBottonRight : FrameLayout = findViewById(R.id.fragment_container_bottomright)
         frameBottonRight.setOnClickListener(){
             bipColor(frameBottonRight, R.color.green)
+            compareSaisieSequence(3)
             println("on a clické sur frameBottonRight vert")
         }
 
@@ -56,6 +47,61 @@ class GameActivity : FragmentActivity() {
             .replace(R.id.fragment_container_bottomleft, FirstFragment())
             .replace(R.id.fragment_container_bottomright, FirstFragment())
             .commit()
+
+
+        //mettre en place l'alternance tourMachine/tourJoueur
+
+        tourMachine()
+
+
+
+
+
+    }
+
+    fun tourMachine(){
+        //choix aléatoire d'un frame
+        //méthode de random 0-3
+        val random = Utilitaires.generateRandom(4)
+        sequence.add(random)
+
+        for (value in sequence){
+            bipColorFromInt(value)
+        }
+
+        //attente de reproduction par l'utilisateur
+        //validation ou non de la séquence
+        //recommence à partir de la séquence enregistrée ou à partir de zero
+
+    }
+
+    fun tourJoueur(){
+        //saisie joueur
+        //comparaison sequence
+
+        //boolean tour du joueur actif
+
+        compteur = 0
+    }
+
+    fun compareSaisieSequence(saisieUtil : Int){
+        if(sequence.get(compteur)==saisieUtil){
+            //on est content
+            compteur++
+        }
+        else{
+            //fin de partie
+            compteur = 0
+        }
+    }
+
+    private fun bipColorFromInt(value: Int) {
+        when(value){
+            0 -> bipColor(findViewById(R.id.fragment_container_topleft), R.color.blue)
+            1 -> bipColor(findViewById(R.id.fragment_container_topright), R.color.red)
+            2 -> bipColor(findViewById(R.id.fragment_container_bottomleft), R.color.yellow)
+            3 -> bipColor(findViewById(R.id.fragment_container_bottomright), R.color.green)
+        }
     }
 
     fun bipColor(frameToBip : FrameLayout, color : Int){
