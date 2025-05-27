392e80fc827dfc72f347da0c0a03401a944ead6b
MaDes <madesappli@gmail.com>
2025-05-27 15:04:36 +0200

    TourMachine TourJoueur partiel

--- app/src/main/java/com/dev/testappcdastdenis/GameActivity.kt
+++ app/src/main/java/com/dev/testappcdastdenis/GameActivity.kt
@@ -1,6 +1,7 @@
 package com.dev.testappcdastdenis
 
 import android.os.Bundle
+import android.os.Handler
 import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
@@ -10,6 +11,7 @@ class GameActivity : FragmentActivity() {
 
     private var sequence = mutableListOf<Int>()
     var compteur = 0
+    var tourJoueurOn = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,26 +20,38 @@ class GameActivity : FragmentActivity() {
 
         val frameTopLeft : FrameLayout = findViewById(R.id.fragment_container_topleft)
         frameTopLeft.setOnClickListener(){
-            bipColor(frameTopLeft, R.color.blue)
-            compareSaisieSequence(0)
+            if(tourJoueurOn) {
+                bipColor(frameTopLeft, R.color.blue)
+                compareSaisieSequence(0)
+                checkFinTour()
+            }
             println("on a clické sur topleft bleu")
         }
         val frameTopRight : FrameLayout = findViewById(R.id.fragment_container_topright)
         frameTopRight.setOnClickListener(){
-            bipColor(frameTopRight, R.color.red)
-            compareSaisieSequence(1)
+            if(tourJoueurOn){
+                bipColor(frameTopRight, R.color.red)
+                compareSaisieSequence(1)
+                checkFinTour()
+            }
             println("on a clické sur frameTopRight rouge")
         }
         val frameBottomLeft : FrameLayout = findViewById(R.id.fragment_container_bottomleft)
         frameBottomLeft.setOnClickListener(){
-            bipColor(frameBottomLeft, R.color.yellow)
-            compareSaisieSequence(2)
+            if(tourJoueurOn) {
+                bipColor(frameBottomLeft, R.color.yellow)
+                compareSaisieSequence(2)
+                checkFinTour()
+            }
             println("on a clické sur frameBottomLeft jaune")
         }
         val frameBottonRight : FrameLayout = findViewById(R.id.fragment_container_bottomright)
         frameBottonRight.setOnClickListener(){
-            bipColor(frameBottonRight, R.color.green)
-            compareSaisieSequence(3)
+            if(tourJoueurOn) {
+                bipColor(frameBottonRight, R.color.green)
+                compareSaisieSequence(3)
+                checkFinTour()
+            }
             println("on a clické sur frameBottonRight vert")
         }
 
@@ -51,27 +65,40 @@ class GameActivity : FragmentActivity() {
 
         //mettre en place l'alternance tourMachine/tourJoueur
 
+       // Thread.sleep(1000)
         tourMachine()
+    }
 
+    private fun checkFinTour() {
+        if(sequence.size==compteur){
+            tourJoueurOn=false
+            tourMachine()
+        }
+        else {
 
-
-
-
+        }
     }
 
     fun tourMachine(){
+        compteur = 0
         //choix aléatoire d'un frame
         //méthode de random 0-3
         val random = Utilitaires.generateRandom(4)
         sequence.add(random)
-
+      /*  sequence.add(2)
+        sequence.add(3)
+        sequence.add(1)
+        sequence.add(2)
+*/
         for (value in sequence){
             bipColorFromInt(value)
         }
 
+        tourJoueurOn = true;
         //attente de reproduction par l'utilisateur
         //validation ou non de la séquence
         //recommence à partir de la séquence enregistrée ou à partir de zero
+        println("fin tour machine")
 
     }
 
@@ -82,17 +109,20 @@ class GameActivity : FragmentActivity() {
         //boolean tour du joueur actif
 
         compteur = 0
+        tourJoueurOn = false
+        println("fin tour joueur")
+
     }
 
     fun compareSaisieSequence(saisieUtil : Int){
         if(sequence.get(compteur)==saisieUtil){
-            //on est content
             compteur++
         }
         else{
             //fin de partie
             compteur = 0
         }
+        println("compteur : $compteur")
     }
 
     private fun bipColorFromInt(value: Int) {
@@ -106,9 +136,11 @@ class GameActivity : FragmentActivity() {
 
     fun bipColor(frameToBip : FrameLayout, color : Int){
         frameToBip.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-        frameToBip.postDelayed({
-            frameToBip.setBackgroundColor(ContextCompat.getColor(this, color))
-        }, 500)
+
+        //Thread.sleep(500)
+         frameToBip.postDelayed({
+             frameToBip.setBackgroundColor(ContextCompat.getColor(this, color))
+         }, 500)
     }
 
 }
